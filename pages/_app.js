c4b37e8c872713f6be1aa4c2ed9db6acac7784e3
nali-kang <softnali92@gmail.com>
2024-06-05 22:38:30 +0900

    React Partal 사용

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
+import Head from "next/head";
+
 import "../styles/globals.css";
 import Layout from "../components/layout/layout";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <meta name="viewport" content="with=device-width, initial-scale=1" />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
