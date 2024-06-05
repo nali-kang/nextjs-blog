5fa0f0122b2548e147a204f996083ec35e96f9fc
nali-kang <softnali92@gmail.com>
2024-06-05 17:48:32 +0900

    MongoDB 메세지 저장

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,6 @@
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
   if (req.method === "POST") {
     const { email, name, message } = req.body;
 
@@ -19,9 +21,30 @@ function handler(req, res) {
       message,
     };
 
-    console.log(newMessage);
+    let client;
 
-    res.status(201).json({ message: "ohohohohoh" });
+    try {
+      client = await MongoClient.connect(
+        "mongodb+srv://softnali92:7qAzQuHCvdEYmMC5@cluster0.1voxfyl.mongodb.net/my-site?retryWrites=true&w=majority&appName=Cluster0"
+      );
+    } catch (error) {
+      res.status(500).json({ meassage: "Could not" });
+      return;
+    }
+
+    const db = client.db();
+
+    try {
+      const result = db.collection("message").insertOne(newMessage);
+      newMessage.id = (await result).insertedId;
+    } catch (error) {
+      client.close();
+      res.sataus(500).json({ message: "failed !" });
+      return;
+    }
+    client.close();
+
+    res.status(201).json({ message: "Success !", message: newMessage });
   }
 }
 
