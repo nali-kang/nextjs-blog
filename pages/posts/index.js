52ecdc97a8e26580523d3abedab2e0253346604a
nali-kang <softnali92@gmail.com>
2024-06-05 22:28:48 +0900

    Head 데이터 추가

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,9 +1,19 @@
 import { getClientBuildManifest } from "next/dist/client/route-loader";
+import Head from "next/head";
+import { Fragment } from "react";
 import AllPosts from "../../components/posts/all-posts";
 import { getAllPosts } from "../../lib/posts-util";
 
 function AllPostsPage(props) {
-  return <AllPosts posts={props.posts} />;
+  return (
+    <Fragment>
+      <Head>
+        <title>All Posts</title>
+        <meta name="description" content="posts!" />
+      </Head>
+      <AllPosts posts={props.posts} />{" "}
+    </Fragment>
+  );
 }
 
 export function getStaticProps() {
